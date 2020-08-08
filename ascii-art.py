3a6346770db1a26dd1d4b42076046ae91564d5c9
Raymond <wrg7yu@gmail.com>
2020-08-07 22:15:49 -0400

    Change grayscale calculation and add resize image functionality

--- ascii-art.py
+++ ascii-art.py
@@ -4,10 +4,12 @@ im = Image.open("pineapple-on-beach.jpg")
 image_array = np.array(im)
 # print(image_array.shape)
 
+while(im.size[0] > 700 or im.size[1]>400):
+    im = im.resize((round(im.size[0]*0.75), round(im.size[1]*0.75)))
 char_list = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 for y in image_array:
     for x in y:
-        x = (x[0] + x[1] + x[2]) // 3
+        x = round(0.21*x[0] + 0.72*x[1] + 0.07*x[2])
         print(char_list[x // 4], end='')
         print(char_list[x // 4], end='')
     print('')
