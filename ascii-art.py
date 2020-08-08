b6b7af9e4e40e63ec8dda8e6fb56c118871ec7ec
Raymond <wrg7yu@gmail.com>
2020-08-07 22:19:17 -0400

    Image source is created by local machine camera

--- ascii-art.py
+++ ascii-art.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python3
 from PIL import Image
+from subprocess import Popen, PIPE
 import numpy as np
-im = Image.open("pineapple-on-beach.jpg")
-image_array = np.array(im)
-# print(image_array.shape)
+import time
+
+p1 = Popen(["imagesnap","-w","2","snapshot.jpg"], stdout=PIPE)
+time.sleep(3)
+im = Image.open("snapshot.jpg")
 
 while(im.size[0] > 700 or im.size[1]>400):
     im = im.resize((round(im.size[0]*0.75), round(im.size[1]*0.75)))
+image_array = np.array(im)
+
 char_list = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 for y in image_array:
     for x in y:
