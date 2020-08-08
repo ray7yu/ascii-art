aebb11c40f7bd2e0f2bdad1b4e45578e845b7c74
Raymond <wrg7yu@gmail.com>
2020-08-07 22:07:21 -0400

    Set up ASCII mapping and calculate average whitespace value for each pixel

--- ascii-art.py
+++ ascii-art.py
@@ -3,3 +3,10 @@ import numpy as np
 im = Image.open("pineapple-on-beach.jpg")
 image_array = np.array(im)
 # print(image_array.shape)
+
+char_list = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+for y in image_array:
+    for x in y:
+        x = (x[0] + x[1] + x[2]) // 3
+        print(char_list[x // 4], end='')
+    print('')
